f7965074052795dd661e4a0c64f006cdcca1591d
nikhil <nikhilappari870@gmail.com>
2026-04-06 00:05:12 +0530

    added demo route

--- FinSight-Backend/server.js
+++ FinSight-Backend/server.js
@@ -33,3 +33,18 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("FinSight Backend API is running 🚀");
+});
+
+app.get("/demo", (req, res) => {
+  res.json({
+    message: "FinSight API working",
+    endpoints: [
+      "/api/auth/register",
+      "/api/auth/login",
+      "/api/transactions",
+      "/api/analytics/summary"
+    ]
+  });
+});
